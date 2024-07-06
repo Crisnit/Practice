faabbfc4a5bf4ebda31a68c408a4381bc5e2d748
Crisnit <amatevosan674@gmail.com>
2024-07-06 21:07:34 +0500

    Record class improved

--- Record.hpp
+++ Record.hpp
@@ -9,18 +9,39 @@ union ValueType{
     char ch[32];
 };
 
+template <typename T>
 struct Record{
-    public:
-    enum Type {fl, db, i, ui, ch};
-    Record(uint32_t id, char name[64], uint64_t timestamp, Type type, ValueType value): id(id), timestamp(timestamp), type(type)
+public:
+    enum Type {float_type, double_type, int_type, uint_type, char_type};
+    Record(uint32_t id, char name[64], uint64_t timestamp, Type type, T input_value): id(id), timestamp(timestamp), type(type)
     {
         std::memcpy(Name, name, 64);
+        defineValueType(input_value);
     };
-
-    private:
-        uint32_t id;
-        char Name[64];
-        uint64_t timestamp;
-        Type type;
-        ValueType value;
+private:
+    void defineValueType(T input_value){
+        switch (type)
+        {
+        case float_type:
+            value.fl = input_value;
+            break;
+        case double_type:
+            value.db = input_value;
+            break;
+        case int_type:
+            value.i = input_value;
+            break;
+        case uint_type:
+            value.ui = input_value;
+            break;
+        case char_type:
+            std::memcpy(value.ch, input_value, 32);
+            break;
+        }
+    }
+    uint32_t id;
+    char Name[64];
+    uint64_t timestamp;
+    Type type;
+    ValueType value;
 };
