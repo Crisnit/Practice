becff5703323db1ae1e0bcc840f1fa8a8adc792b
Crisnit <amatevosan674@gmail.com>
2024-07-07 18:24:06 +0500

    Some practice with mutex

--- datawarehousestructs.hpp
+++ datawarehousestructs.hpp
@@ -23,6 +23,7 @@ public:
         m_timestamp = getTimestamp();
     };
 
+
 private:
     template <typename T>
     void setValue(T input_value){
@@ -95,79 +96,5 @@ private:
     int m_arr_size;
     int m_arr_count;
     int m_memory_size;
-    int m_elem_size = RECORD_SIZE;
-}
-
-/*class SharedMemoryBuilder{
-public:
-    virtual void buildName(char &name) = 0;
-
-    virtual void buildArrSize(int array_size) = 0;
-
-    virtual void buildMemSize(int array_size) = 0;
-
-    virtual SharedMemory getResult() = 0;
+    int m_elem_size = sizeof(Record);
 };
-
-class ServerSharedMemoryBuilder : public SharedMemoryBuilder{
-public:
-    ServerSharedMemoryBuilder(){
-        m_shared_memory = SharedMemory();
-    }
-
-    void buildName(char &name) override{
-        m_shared_memory.setName(name);
-    }
-
-    void buildArrSize(int array_size) override{
-        m_shared_memory.setArrSize(array_size);
-    }
-
-    void buildMemSize(int array_size) override{
-        int memory_size = array_size * RECORD_SIZE;
-        m_shared_memory.setMemSize(memory_size);
-    }
-
-    SharedMemory getResult() override{
-        return m_shared_memory;
-    }
-    private:
-    SharedMemory m_shared_memory;
-};
-
-class ClientSharedMemoryBuilder : public SharedMemoryBuilder{
-public:
-    ClientSharedMemoryBuilder(){
-        m_shared_memory = SharedMemory();
-    }
-
-    void buildName(char &name) override{
-        m_shared_memory.setName(name);
-    }
-
-    void buildArrSize(int array_size) override{
-        m_shared_memory.setArrSize(array_size);
-    }
-
-    void buildMemSize(int array_size) override{
-        int memory_size = array_size * RECORD_SIZE;
-        m_shared_memory.setMemSize(memory_size);
-    }
-
-    SharedMemory getResult() override{
-        return m_shared_memory;
-    }
-    private:
-    SharedMemory m_shared_memory;
-};
-
-class SHMApi{
-public:
-    SHMApi(){
-        m_shm = SharedMemory();
-    }
-    void buildServer(char &name, int arr_size);
-private:
-    Record* m_ptr;
-    SharedMemory m_shm;
-};*/
