ee5bffebcce2cf884dad4c195fa8bb27331dab12
Crisnit <amatevosan674@gmail.com>
2024-07-09 04:53:20 +0500

    Small changes

--- datawarehousestructs.hpp
+++ datawarehousestructs.hpp
@@ -9,13 +9,14 @@
 #include <fcntl.h>
 #include <iostream>
 
-class SharedMemoryMutex{
+class SharedMemoryMutex {
 public:
+    void init(pthread_mutex_t &t_lock);
     void lock();
     void unlock();
-}
+};
 
-union ValueType{
+union ValueType {
     float fl;
     double db;
     int32_t i;
@@ -26,16 +27,18 @@ union ValueType{
 struct Record{
 public:
     enum Type {float_type, double_type, int_type, uint_type, char_ptr_type};
-    template <typename T> Record(uint32_t id, char name[64], Type type, T input_value): m_id(id), m_type(type)
-    {
+    template <typename T> Record(uint32_t id, char name[64], Type type, T input_value): m_id(id), m_type(type) {
         std::memcpy(m_name, name, 64);
         setValue(input_value);
         m_timestamp = getTimestamp();
     };
 
+    void clear() {
+
+    }
 
     template <typename T>
-    void setValue(T input_value){
+    void setValue(T input_value) {
         switch (m_type)
         {
         case float_type:
@@ -56,7 +59,7 @@ public:
         }
     }
 
-    uint64_t getTimestamp(){
+    uint64_t getTimestamp() {
         std::chrono::_V2::system_clock::duration time = std::chrono::high_resolution_clock::now().time_since_epoch();
         return std::chrono::duration_cast<std::chrono::microseconds>(time).count();
     }
@@ -68,29 +71,29 @@ public:
     ValueType m_value;
 };
 
-struct SharedMemoryData{
+struct SharedMemoryData {
     pthread_mutex_t m_lock;
     uint32_t m_arr_count;
     Record m_arr[0];
-}
+};
 
-class SharedMemory{
+class SharedMemory {
 public:
-    SharedMemory(const std::string& t_name, int entity_num): m_name(t_name), m_arr_count(entity_num){
-        m_shm_fd = shm_open(m_name, O_CREAT | O_EXCL | O_RDWR, 0666);
+    SharedMemory(const std::string& t_name, int t_entity_num): m_name(t_name) {
+        m_shm_fd = shm_open(m_name.data(), O_CREAT | O_EXCL | O_RDWR, 0666);
         if (m_shm_fd < 0) {
             throw std::runtime_error("Shared memory with this name already exists");
         }
 
-        m_memory_size = entity_num * sizeof(Record) + sizeof(SharedMemoryData);
+        m_memory_size = t_entity_num * sizeof(Record) + sizeof(SharedMemoryData);
 
         ftruncate(m_shm_fd, m_memory_size);
 
         m_data_ptr = (SharedMemoryData*)mmap(0, m_memory_size, PROT_READ | PROT_WRITE, MAP_SHARED, m_shm_fd, 0);
-        m_data_ptr->m_arr_count = m_arr_count;
-        m_mutex.init(&m_data_ptr->m_lock);
+        m_data_ptr->m_arr_count = t_entity_num;
+        m_mutex.init(m_data_ptr->m_lock);
 
-        for (size_t i = 0; i < m_arr_count; i++) {
+        for (size_t i = 0; i < t_entity_num; i++) {
             m_data_ptr->m_arr[i].clear();
            std::cout << m_data_ptr->m_arr[i].m_id;
         }
@@ -99,8 +102,10 @@ public:
     void addRecord(const Record &t_record) {
         if (m_arr_size > m_data_ptr->m_arr_count) {
             m_mutex.lock();
+
             m_data_ptr->m_arr[m_data_ptr->m_arr_count] = t_record;
             m_data_ptr->m_arr_count++;
+
             m_mutex.unlock();
         }
     }
@@ -109,14 +114,14 @@ public:
         if (m_data_ptr->m_arr_count > 0) {
             m_mutex.lock();
             for (size_t i = 0; i < m_data_ptr->m_arr_count; i++) {
-                std::cout << &m_arr_ptr[i] << std::endl;
+                std::cout << &m_data_ptr->m_arr[i] << std::endl;
             }
             m_mutex.unlock();
         }
     }
 
     ~SharedMemory() {
-        shm_unlink(m_name);
+        shm_unlink(m_name.data());
     }
 
 private:
