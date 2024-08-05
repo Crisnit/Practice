aa597f1c3cff130ab76ac59ecf3a067ff81d7f24
Crisnit <amatevosan674@gmail.com>
2024-08-05 15:15:42 +0500

    Some improvements

--- bash_script.sh
+++ bash_script.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
 if [ $# -eq 2 ]
+
 then
 memoryName=$1
 arrayCapacity=$2
 cd bin 
 ./shm_server $memoryName $arrayCapacity &
 sleep 0.1
+
 for ((i=0; i < 5; i++))
 do
 ./shm_client $memoryName --show
 echo "_________________________________________________________________"
 sleep 5
 done
+
 else
 echo "Wrong number of parameters"
 fi
